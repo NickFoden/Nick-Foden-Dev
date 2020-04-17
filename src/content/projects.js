f06fd38ba9f204d1dcb29e4be73d5b01a559ec22
Nick Foden <nickfoden@gmail.com>
2020-04-17 11:16:28 -0400

    Bump wallplay down and add out of production information

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,7 +1,9 @@
 import Cards from "../images/cards.png"
+import ExploreJS from "../images/exJSLogo.png"
 import FlashFauna from "../images/flash-fauna.png"
 import FodenDev from "../images/fodenDev.png"
 import Icon from "../images/theiconorg.png"
+import googleTranslate from "../images/googleTranslateClient.png"
 import Sustain from "../images/sustain.png"
 import StateYourPlate from "../images/StateYourPlate.jpeg"
 import Tommy from "../images/tommy_crab_green.png"
@@ -11,46 +13,24 @@ import UseReact from "../images/useReactNYC.png"
 export default [
   {
     id: "1",
-    image: Wallplay,
-    title: "wallplay network",
-    subTitle: "A marketplace for pop-up experiences",
-    summary:
-      "2 React apps. A public facing platform and an internal cms for editing the content, managing the projects, the spaces and the users and their profiles. Both React apps sharing a Node back end and a NoSQL Database in Google Cloud.",
-    siteLink: "https://wallplay.com/",
-    sourceLink: "",
+    image: ExploreJS,
+    title: "Explore JS",
+    subTitle: "JavaScript and Css collaborative mentorship.",
+    summary: `ExploreJS is a project to scale mentoring for new developers/students who are learning JavaScript. Offering free "office hours" for anyone to come and debug their code over screenshare with intermediate and advanced developer mentors as well as be a community for shared ideas and resources. Coding Coach started a lot of mentoring relationships, and at a point it's hard to mentor more than a few people at a time. The goal is to ultimately help new developers go from student to professional by working together.`,
+    siteLink: "https://explorejs.com/",
+    sourceLink: "https://github.com/explorejs/explorejs",
   },
   {
     id: "2",
-    image: Icon,
-    title: "Industrial & Commerical Operations Network",
-    subTitle: "The definitive tool for energy professionals",
-    summary:
-      "A platform for Energy Utility information and compliance in the UK. The platform keeps users up to date on latest Energy news, allows them to check they are compliant with the current and future regulations and chat in forums about Energy issues.",
-    siteLink: "https://theicon.org.uk/",
-    sourceLink: "",
+    image: googleTranslate,
+    title: "google-translate-client",
+    subTitle: "NPM package for google translate",
+    summary: `NPM has a few google translate options, and after seeing how they didn't work for people. I decided to publish my own "simple" client that takes text and a language and returns the translation. Easy to use. Simple.`,
+    siteLink: "https://www.npmjs.com/package/google-translate-client",
+    sourceLink: "https://github.com/NickFoden/google-translate-client",
   },
   {
     id: "3",
-    image: Sustain,
-    title: "Sustain Investing",
-    subTitle: "A community-driven investment fund",
-    summary:
-      "Formed a company with few like minded individuals, where users are voting on the sustainablity of public companies actions under a context of a few different lenses. I built the community platform for web and the (near approved) React Native app for AppStore/GooglePlay.",
-    siteLink: "https://app.sustaininvesting.com/",
-    sourceLink: "",
-  },
-  {
-    id: "4",
-    image: Tommy,
-    title: "Tommy's Cartoons",
-    subTitle: "View 500 days of Tommy's cartoons",
-    summary:
-      "Tommy Siegel drew and posted online a cartoon every day for 500 days. I built this React App so fans can cycle randomly or skip to a specific date and see all of Tommy's 500 Cartoons in one place.",
-    siteLink: "https://tommysiegelcartoons.netlify.com/",
-    sourceLink: "",
-  },
-  {
-    id: "5",
     image: UseReact,
     title: "useReactNYC",
     subTitle: "Website 2.0 for NY's newest and most exciting React Meetup",
@@ -60,17 +40,28 @@ export default [
     sourceLink: "https://github.com/usereactnyc/usereact.nyc",
   },
   {
-    id: "6",
-    image: FodenDev,
-    title: "Foden Dev Blog",
-    subTitle: "Blogging up in here",
+    id: "4",
+    image: Sustain,
+    title: "Sustain Investing",
+    subTitle: "A community-driven investment fund",
     summary:
-      "Myself and 2 of my cousins in other countries are working in web dev, and I started this blog as a way for us all to blog what we learn and share with the web. More posts to come. Many more.",
-    siteLink: "https://foden.dev/",
-    sourceLink: "https://github.com/NickFoden/foden-dev-blog",
+      "Formed a company with few like minded individuals, where users are voting on the sustainablity of public companies actions under a context of a few different lenses. I built the community platform for web and the (near approved) React Native app for AppStore/GooglePlay.",
+    siteLink: "https://app.sustaininvesting.com/",
+    sourceLink: "",
   },
   {
-    id: "7",
+    id: "5",
+    image: Tommy,
+    title: "Tommy's Cartoons",
+    subTitle: "View 500 days of Tommy's cartoons",
+    summary:
+      "Tommy Siegel drew and posted online a cartoon every day for 500 days. I built this React App so fans can cycle randomly or skip to a specific date and see all of Tommy's 500 Cartoons in one place.",
+    siteLink: "https://tommysiegelcartoons.netlify.com/",
+    sourceLink: "",
+  },
+
+  {
+    id: "6",
     image: FlashFauna,
     title: "Flash Fauna",
     subTitle: "FaunaDB + React flashcards app",
@@ -80,23 +71,35 @@ export default [
     sourceLink: "https://github.com/NickFoden/flash-fauna",
   },
   {
-    id: "8",
-    image: StateYourPlate,
-    title: "State Your Plate",
-    subTitle: "Upload vanity license plates",
+    id: "7",
+    image: Wallplay,
+    title: "wallplay network",
+    subTitle: "A marketplace for pop-up experiences",
     summary:
-      "Fast little app built in 3 hours to try out firestore when it was new. React + Bulma CSS + Firestore. Have resumed working on a React Native version to be complete August 2019. ",
-    siteLink: "https://stateyourplate.co",
+      "2 React apps. A public facing platform and an internal cms for editing the content, managing the projects, the spaces and the users and their profiles. Both React apps sharing a Node back end and a NoSQL Database in Google Cloud.",
+    sourceInfo: "Out of production",
+    siteLink: "",
     sourceLink: "",
   },
   {
-    id: "9",
-    image: Cards,
-    title: "Cards",
-    subTitle: "Node + Pug JavaScript methods flashcards app",
+    id: "8",
+    image: Icon,
+    title: "Industrial & Commerical Operations Network",
+    subTitle: "The definitive tool for energy professionals",
     summary:
-      "This node app project is supported by a mongo db at mLab and the code is deployed on HEROKU. PassportJS handle user authentication and if they are succesfully logged in then users have option to add more cards.",
-    siteLink: " https://cards-js.herokuapp.com/",
-    sourceLink: "https://github.com/NickFoden/cards-pug",
+      "A platform for Energy Utility information and compliance in the UK. The platform keeps users up to date on latest Energy news, allows them to check they are compliant with the current and future regulations and chat in forums about Energy issues.",
+    siteLink: "https://theicon.org.uk/",
+    sourceLink: "",
+  },
+
+  {
+    id: "9",
+    image: FodenDev,
+    title: "Foden Dev Blog",
+    subTitle: "Blogging up in here",
+    summary:
+      "Myself and 2 of my cousins in other countries are working in web dev, and I started this blog as a way for us all to blog what we learn and share with the web. More posts to come. Many more.",
+    siteLink: "https://foden.dev/",
+    sourceLink: "https://github.com/NickFoden/foden-dev-blog",
   },
 ]
